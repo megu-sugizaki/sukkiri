934b757d3e775261d4b7d6cbbb538830fad0fc81
megumi <megusugizaki@gmail.com>
2024-01-29 18:02:07 +0900

    p24 0-5

--- src/sukkiri/Main.java
+++ src/sukkiri/Main.java
@@ -1,10 +1,10 @@
 package sukkiri;
 
 public class Main {
-
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-		System.out.println("Hello World!!");
+	  public static void main(String[] args) { 
+	    System.out.println(35 - 10);
+	    System.out.println(-5 * 2);
+	    System.out.println(6 * 6 * 3.14);
+	    System.out.println("こたえは" + 64);
+	  }
 	}
-
-}
