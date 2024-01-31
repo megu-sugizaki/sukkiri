a6cdb3f02ebc35a3ee0ca4d6105c32afd68deeb9
megumi <megusugizaki@gmail.com>
2024-01-31 20:32:09 +0900

    practice 1-3

--- src/sukkiri/Main.java
+++ src/sukkiri/Main.java
@@ -2,9 +2,10 @@ package sukkiri;
 
 public class Main {
 	  public static void main(String[] args) { 
-		  int a = 3;
-		  int b = 5;
-		  System.out.println("縦幅" + a + "横幅" + b +"の長方形の面積は、" + a * b );
-		  
+		  boolean a = true;
+		  char b = '駆';
+		  final double PI = 3.14;
+		  long c = 314159265853979L;
+		  String d = "ミナトの攻撃！的に15ポイントのダメージを与えた！";
 	    }
 	}
