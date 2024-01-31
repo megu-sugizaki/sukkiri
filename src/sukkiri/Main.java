df587580f3f2caa809a2956fc3b65a008672b9e3
megumi <megusugizaki@gmail.com>
2024-01-31 20:27:59 +0900

    practice 1-2

--- src/sukkiri/Main.java
+++ src/sukkiri/Main.java
@@ -2,9 +2,9 @@ package sukkiri;
 
 public class Main {
 	  public static void main(String[] args) { 
-		  final double PI = 3.14;
-		  double pie = 5;
-		  System.out.println("半径" + pie + "cm" + "のパイの面積は");
-		  System.out.println(pie * pie * PI);
+		  int a = 3;
+		  int b = 5;
+		  System.out.println("縦幅" + a + "横幅" + b +"の長方形の面積は、" + a * b );
+		  
 	    }
 	}
