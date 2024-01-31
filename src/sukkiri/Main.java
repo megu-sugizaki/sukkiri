53cef402c92ec3a9acacf22a35c1d069ef462e85
megumi <megusugizaki@gmail.com>
2024-01-31 17:54:07 +0900

    1-2

--- src/sukkiri/Main.java
+++ src/sukkiri/Main.java
@@ -2,12 +2,8 @@ package sukkiri;
 
 public class Main {
 	  public static void main(String[] args) { 
-	    System.out.println(35 - 10);
-	    System.out.println(-5 * 2);
-	    System.out.println(6 * 6 * 3.14);
-	    System.out.println("こたえは" + 64);
-	    int x;
-	    x = 6;
-	    System.out.println(x * x *3.14);
+	    int age;
+	    age = 30;
+	    System.out.println(age);
 	    }
 	}
