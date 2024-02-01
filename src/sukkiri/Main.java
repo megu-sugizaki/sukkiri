174bedf23e7457d83526321972ed3d9c19f1ae45
megumi <megusugizaki@gmail.com>
2024-02-01 17:04:06 +0900

    2-9

--- src/sukkiri/Main.java
+++ src/sukkiri/Main.java
@@ -2,9 +2,7 @@ package sukkiri;
 
 public class Main {
 	  public static void main(String[] args) { 
-		 double d = 8.5 / 2 ;
-		 long l = 5 + 2L;
-		 System.out.println(d);
-		 System.out.println(l);
+		 String msg = "私の年齢は" + 23;
+		 System.out.println(msg);
 	    }
 	}
