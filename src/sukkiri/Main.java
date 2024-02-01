c4be4569ca9000830de9b8597e1db1be0f49057d
megumi <megusugizaki@gmail.com>
2024-02-01 10:45:08 +0900

    2-1

--- src/sukkiri/Main.java
+++ src/sukkiri/Main.java
@@ -2,10 +2,11 @@ package sukkiri;
 
 public class Main {
 	  public static void main(String[] args) { 
-		  boolean a = true;
-		  char b = '駆';
-		  final double PI = 3.14;
-		  long c = 314159265853979L;
-		  String d = "ミナトの攻撃！的に15ポイントのダメージを与えた！";
+		  int a;
+		  int b;
+		  a = 20;
+		  b = a + 5;
+		  System.out.println(a);
+		  System.out.println(b);
 	    }
 	}
