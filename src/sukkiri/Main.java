8d7671a15002e6fa6c58aecd400c6edf589c2c9e
megumi <megusugizaki@gmail.com>
2024-02-01 11:54:03 +0900

    2-4

--- src/sukkiri/Main.java
+++ src/sukkiri/Main.java
@@ -3,10 +3,8 @@ package sukkiri;
 public class Main {
 	  public static void main(String[] args) { 
 		  int a;
-		  int b;
-		  a = 20;
-		  b = a + 5;
+		  a = 100;
+		  a++;
 		  System.out.println(a);
-		  System.out.println(b);
 	    }
 	}
