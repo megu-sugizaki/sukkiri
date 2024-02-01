dac707932c9fa7253a4dab5630733bccecddc2bd
megumi <megusugizaki@gmail.com>
2024-02-01 17:59:15 +0900

    2-15

--- src/sukkiri/Main.java
+++ src/sukkiri/Main.java
@@ -2,8 +2,11 @@ package sukkiri;
 
 public class Main {
 	  public static void main(String[] args) { 
-//		  ランダムに数字を出す
-		  int r = new java.util.Random().nextInt(90);
-		  System.out.println("あなたはたぶん、" + r + "歳ですね?"  );
+//		  年齢と名前の入力（Progateと違ってimportは使用しない）
+		  System.out.println("あなたの名前を入力してください。");
+		  String name = new java.util.Scanner(System.in).nextLine();
+		  System.out.println("あなたの年齢を入力してください");
+		  int age = new java.util.Scanner(System.in).nextInt();
+		  System.out.println("ようこそ、" + age + "歳の"  + name + "さん");
 	    }
 	}
