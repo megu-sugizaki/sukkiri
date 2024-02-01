aba49303064f957779104d34b3b4f581a7706b4d
megumi <megusugizaki@gmail.com>
2024-02-01 17:19:08 +0900

    2-11

--- src/sukkiri/Main.java
+++ src/sukkiri/Main.java
@@ -2,9 +2,9 @@ package sukkiri;
 
 public class Main {
 	  public static void main(String[] args) { 
-		  String name = "すがわら";
-		  String message;
-		  message = name + "さん、こんにちは";
-		  System.out.println(message);
+		  String name ="すがわら";
+		  System.out.print("私の名前は");
+		  System.out.print(name);
+		  System.out.print("です");
 	    }
 	}
