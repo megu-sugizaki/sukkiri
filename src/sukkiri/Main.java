4353282bb54df855f76badb823806f1e7b11d24c
megumi <megusugizaki@gmail.com>
2024-02-01 17:30:32 +0900

    2-13

--- src/sukkiri/Main.java
+++ src/sukkiri/Main.java
@@ -2,9 +2,8 @@ package sukkiri;
 
 public class Main {
 	  public static void main(String[] args) { 
-		  int a = 5;
-		  int b = 3;
-		  int m = Math.max(a,  b);
-		  System.out.println("比較実験:" + a + "と" + b + "とで大きい方は…" + m);
+		  String age = "31";
+		  int n = Integer.parseInt(age);
+		  System.out.println("あなたは来年" + (n + 1) + "際になりますね。");
 	    }
 	}
