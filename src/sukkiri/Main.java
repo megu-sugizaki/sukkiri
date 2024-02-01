d9563510c9075ddc471ceedfd49f581e40ce6d75
megumi <megusugizaki@gmail.com>
2024-02-01 21:37:24 +0900

    practice2-1

--- src/sukkiri/Main.java
+++ src/sukkiri/Main.java
@@ -2,11 +2,9 @@ package sukkiri;
 
 public class Main {
 	  public static void main(String[] args) { 
-//		  年齢と名前の入力（Progateと違ってimportは使用しない）
-		  System.out.println("あなたの名前を入力してください。");
-		  String name = new java.util.Scanner(System.in).nextLine();
-		  System.out.println("あなたの年齢を入力してください");
-		  int age = new java.util.Scanner(System.in).nextInt();
-		  System.out.println("ようこそ、" + age + "歳の"  + name + "さん");
+		  int x = 5;
+		  int y = 10;
+		  String ans = "x+yは" + (x + y);
+		  System.out.println(ans);
 	    }
 	}
