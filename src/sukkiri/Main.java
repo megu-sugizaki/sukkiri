1f0fd3b3232c3e0ad7f3494a41941ae13e0074db
megumi <megusugizaki@gmail.com>
2024-02-02 15:02:21 +0900

    3-6

--- src/sukkiri/Main.java
+++ src/sukkiri/Main.java
@@ -3,15 +3,18 @@ package sukkiri;
 public class Main {
 	  public static void main(String[] args) { 
 		  System.out.println("あなたの運勢を占います");
-		  int fortune = new java.util.Random().nextInt(4) + 1;
-		  if (fortune == 1) {
-			  System.out.println("大吉");
-		  } else if (fortune == 2) {
-			  System.out.println("中吉");
-		  } else if (fortune == 3) {
-			  System.out.println("吉");
-		  } else {
-			  System.out.println("凶");
+		  int fortune = new java.util.Random().nextInt(5) + 1;
+		  switch(fortune) {
+		  case 1:
+		  case 2:
+			   System.out.println("いいね!");
+			   break;
+		  case 3:
+			   System.out.println("普通です");
+			   break;
+		  case 4:
+		  case 5:
+			  System.out.println("うーん...");
 		  }
 	    }
 	}
