19f4db35ded7761d4a1af6b7bb701338de4f9283
megumi <megusugizaki@gmail.com>
2024-02-02 13:21:25 +0900

    3-1

--- src/sukkiri/Main.java
+++ src/sukkiri/Main.java
@@ -2,16 +2,12 @@ package sukkiri;
 
 public class Main {
 	  public static void main(String[] args) { 
-		  System.out.println("ようこそ占いの館へ");
-		  System.out.print("あなたの名前を入力してください");
-		  String name = new java.util.Scanner(System.in).nextLine();
-		  System.out.println("あなたの年齢を入力してください");
-		  String agesString = new java.util.Scanner(System.in).nextLine();
-		  int age = Integer.parseInt(agesString);
-		  int fortune = new java.util.Random().nextInt(4);
-		  fortune++;
-		  System.out.println("占いの結果が出ました");
-		  System.out.println(age + "歳の" + name + "さん、あなたの運気番号は" + fortune + "です");
-		  System.out.println("1:大吉 2:中吉 3:吉 4:凶");
+		  boolean tenki = true;
+		  if (tenki == true) {
+			  System.out.println("洗濯をします");
+			  System.out.println("散歩に行きます");
+		  } else {
+			  System.out.println("DVDを見ます");
+		  }
 	    }
 	}
