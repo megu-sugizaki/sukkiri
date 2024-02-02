3f85d85d9a00cfc8ad2355937632b99d7f777925
megumi <megusugizaki@gmail.com>
2024-02-02 18:18:56 +0900

    3-6-2

--- src/sukkiri/Main.java
+++ src/sukkiri/Main.java
@@ -3,11 +3,10 @@ package sukkiri;
 public class Main {
 	  public static void main(String[] args) { 
 		 for (int i = 1; i < 10; i++) {
-			 for (int j = 1; j< 10; j++ ) {
-				 System.out.println(i * j);
-				 System.out.println(" ");
+			 if(i == 3) {
+				 continue;
 			 }
-			 System.out.println(" ");
+			 System.out.println(i);
 		 }
 	    }
 	}
