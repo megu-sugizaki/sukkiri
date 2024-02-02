26573bbf88937c51c2826bb431b046ed44d37998
megumi <megusugizaki@gmail.com>
2024-02-02 14:50:24 +0900

    3-4

--- src/sukkiri/Main.java
+++ src/sukkiri/Main.java
@@ -2,11 +2,16 @@ package sukkiri;
 
 public class Main {
 	  public static void main(String[] args) { 
-//		  無限ループになるので実行しない
-		  boolean doorClose = true;
-		  while (doorClose == true) {
-			  System.out.println("ノックする");
-			  System.out.println("待つ");
+		  System.out.println("あなたの運勢を占います");
+		  int fortune = new java.util.Random().nextInt(4) + 1;
+		  if (fortune == 1) {
+			  System.out.println("大吉");
+		  } else if (fortune == 2) {
+			  System.out.println("中吉");
+		  } else if (fortune == 3) {
+			  System.out.println("吉");
+		  } else {
+			  System.out.println("凶");
 		  }
 	    }
 	}
