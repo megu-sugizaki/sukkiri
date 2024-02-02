eaa26c7fa7766667654d1eeb56afb929f7fe89da
megumi <megusugizaki@gmail.com>
2024-02-02 13:25:41 +0900

    3-2

--- src/sukkiri/Main.java
+++ src/sukkiri/Main.java
@@ -2,12 +2,11 @@ package sukkiri;
 
 public class Main {
 	  public static void main(String[] args) { 
-		  boolean tenki = true;
-		  if (tenki == true) {
-			  System.out.println("洗濯をします");
-			  System.out.println("散歩に行きます");
-		  } else {
-			  System.out.println("DVDを見ます");
+//		  無限ループになるので実行しない
+		  boolean doorClose = true;
+		  while (doorClose == true) {
+			  System.out.println("ノックする");
+			  System.out.println("待つ");
 		  }
 	    }
 	}
