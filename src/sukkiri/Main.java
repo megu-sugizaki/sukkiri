26fb2356bcef913e16b28f0048c8c9025ae48ae4
megumi <megusugizaki@gmail.com>
2024-02-04 20:08:29 +0900

    [extra practice]1 +

--- src/sukkiri/Main.java
+++ src/sukkiri/Main.java
@@ -3,6 +3,7 @@ package sukkiri;
 public class Main {
 	  public static void main(String[] args) { 
 //		  int 型の変数 x、y にそれぞれ数値を入力し、x が y より大きい場合に、“xはyより大きい。”という文を表示するプログラムを作成しなさい。
+//		  https://kitako.tokyo/lib/JavaExercise.aspx?id=3
 		 int x = 8;
 		 int y = 7;
 		 if(x > y) {
