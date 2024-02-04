b2a68589ec7896c6f1317b629c2fb518e0c9bb8a
megumi <megusugizaki@gmail.com>
2024-02-04 21:00:13 +0900

    [extra practice]6

--- src/sukkiri/Main.java
+++ src/sukkiri/Main.java
@@ -2,13 +2,17 @@ package sukkiri;
 
 public class Main {
 	  public static void main(String[] args) { 
-//		  正の整数値を入力し、それが偶数か奇数かを判定するプログラムを作成しなさい。
-		  int x = 4;
-		if(x % 2 == 0) {
-			System.out.println("偶数");
-		} else if (x % 2 == 1) {
-			System.out.println("奇数");
-		}
+//		  整数値を入力し、以下の４つの分類から該当するものを表示するプログラムを作成しなさい。“正の偶数”、“正の奇数”、“負の偶数”、“負の奇数”※ 	負の数であっても、2 で割り切れれば偶数、そうでなければ奇数とする※ 	0 は“正の偶数”であるとする。
+		 int x = 5;
+		  if (x >= 0 && x % 2 == 0) {
+			  System.out.println("正の偶数");
+		  } else if (x >= 0 && x % 2 == 1){
+			  System.out.println("正の奇数");
+		  } else if (x < 0 && x % 2 == 0) {
+			  System.out.println("負の偶数");
+		  } else if (x < 0 && x % 2 == 1) {
+			  System.out.println("負の奇数");
+		  }
 		  
 	    }
 	}
