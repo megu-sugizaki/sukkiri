6a0d93ebc70bc76607ee55bc5d2954d10e878da8
megumi <megusugizaki@gmail.com>
2024-02-04 21:15:58 +0900

    [extra practice]7

--- src/sukkiri/Main.java
+++ src/sukkiri/Main.java
@@ -2,17 +2,50 @@ package sukkiri;
 
 public class Main {
 	  public static void main(String[] args) { 
-//		  整数値を入力し、以下の４つの分類から該当するものを表示するプログラムを作成しなさい。“正の偶数”、“正の奇数”、“負の偶数”、“負の奇数”※ 	負の数であっても、2 で割り切れれば偶数、そうでなければ奇数とする※ 	0 は“正の偶数”であるとする。
-		 int x = 5;
-		  if (x >= 0 && x % 2 == 0) {
-			  System.out.println("正の偶数");
-		  } else if (x >= 0 && x % 2 == 1){
-			  System.out.println("正の奇数");
-		  } else if (x < 0 && x % 2 == 0) {
-			  System.out.println("負の偶数");
-		  } else if (x < 0 && x % 2 == 1) {
-			  System.out.println("負の奇数");
+//		  試験の点数を入力し、対応する成績を表示するプログラムを３種類作成しなさい。
+
+//		  試験は 100 点満点（ 0 点～ 100 点）とし、点数と成績の対応を以下のようにします。
+//		  ケース1
+//		      60 点以上：“合格”
+//		      60 点未満：“不合格"
+//		  ケース2
+//		      80 点以上：“たいへんよくできました。”
+//		      60 点以上、80 点未満：“よくできました。”
+//		      60 点未満：“ざんねんでした。”
+//		  ケース3
+//		      80 点以上：“優”
+//		      70 点以上、80 点未満：“良”
+//		      60 点以上、70 点未満：“可”
+//		      60 点未満：“不可”
+		  int num = 3;
+		  int x = 78;
+		  switch(num) {
+		  case 1:
+			  if(x >= 60) {
+				  System.out.println("合格");
+			  } else if (x < 60) {
+				  System.out.println("不合格");
+			  }
+		  break;
+		  case 2:
+			  if(x > 80) {
+				  System.out.println("たいへんよくできました。");
+			  } else if (x > 60 && x < 80) {
+				  System.out.println("よくできました。");
+			  } else if (x < 60) {
+				  System.out.println("ざんねんでした。");
+			  }
+		  break;
+		  case 3:
+			  if(x >= 80) {
+				  System.out.println("優");
+			  } else if (x >= 70 && x < 80) {
+				  System.out.println("良");
+			  } else if (x >= 60 && x < 70) {
+				  System.out.println("可");
+			  } else if (x < 60) {
+				  System.out.println("不可");
+			  }
 		  }
-		  
 	    }
 	}
