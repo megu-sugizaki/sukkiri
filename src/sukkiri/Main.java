36886c984b8c601fd2ecd6dc1064952ead730f29
megumi <megusugizaki@gmail.com>
2024-02-05 18:38:44 +0900

    4-9

--- src/sukkiri/Main.java
+++ src/sukkiri/Main.java
@@ -2,8 +2,9 @@ package sukkiri;
 
 public class Main {
 	  public static void main(String[] args) { 
-		 int[] scores = new int[5];
-		 scores[1] = 30;
-		 System.out.println(scores[1]);
+		 int[] scores = {20, 30, 40, 50, 80};
+		 for (int i = 0; i < scores.length; i++) {
+			 System.out.println(scores[i]);
+		 }
 	    }
 	}
