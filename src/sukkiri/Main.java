7112b90e282e3923af5b855b15f590a32471ff23
megumi <megusugizaki@gmail.com>
2024-02-06 19:16:40 +0900

    practice 4-2

--- src/sukkiri/Main.java
+++ src/sukkiri/Main.java
@@ -2,15 +2,13 @@ package sukkiri;
 
 public class Main {
 	  public static void main(String[] args) { 
-		  int[] points;
-		  points = new int[4];
+		  int[] moneyList = {121902, 8302, 55100};
+		  for (int i = 0; i < moneyList.length; i ++) {
+			  System.out.println(moneyList[i]);
+		  }
 		  
-		  double[] weights;
-		  weights = new double[5];
-		  
-		  boolean[] answers;
-		  answers = new boolean[3];
-		  
-		  String[] names = new String[3];
+		  for(int value: moneyList) {
+			  System.out.println(value);
+		  }
 	  }
 	}
