fdb0de9b18666968f9ca9cc8a6cda6087ec17eaa
megumi <megusugizaki@gmail.com>
2024-02-07 11:56:16 +0900

    5-5

--- src/sukkiri/Main.java
+++ src/sukkiri/Main.java
@@ -1,15 +1,13 @@
 package sukkiri;
 
 public class Main {
-		 public static void hello(String name) {
-			 System.out.println("こんにちは" + name);
+		 public static void main(String[] args) {
+			 add(100, 20);
+			 add(200, 50);
 		 }
 		 
-		 public static void main(String[] args) {
-			 System.out.println("メソッドを呼び出します");
-			 hello("湊");
-			 hello("朝香");
-			 hello("菅原");
-			 System.out.println("システムの呼び出しが終わりました");
+		 public static void add(int x, int y) {
+			 int ans = x + y;
+			 System.out.println(x + "+" + y + "=" + ans);
 		 }
 	}
