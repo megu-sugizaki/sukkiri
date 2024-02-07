210636bf10dedfc0a8bc12a7e3e8045022a400b6
megumi <megusugizaki@gmail.com>
2024-02-07 12:23:26 +0900

    5-10

--- src/sukkiri/Main.java
+++ src/sukkiri/Main.java
@@ -1,12 +1,21 @@
 package sukkiri;
 
 public class Main {
-		 public static void main(String[] args) {
-			 System.out.println(add(add(10,20), add(30,40)));
+		 public static int add(int x, int y) {
+			 return x + y;
 		 }
 		 
-		 public static int add(int x, int y) {
-			 int ans = x + y;
-			 return ans;
+		 public static double add(double x, double y) {
+			 return x + y;
+		 }
+		 
+		 public static String add(String x, String y) {
+			 return x + y;
+		 }
+		 
+		 public static void main(String[] args) {
+			 System.out.println(add(10, 20));
+			 System.out.println(add(3.5, 2.7));
+			 System.out.println(add("Hello", "World"));
 		 }
 	}
