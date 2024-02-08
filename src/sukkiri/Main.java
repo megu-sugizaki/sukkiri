445976ad0e43c3c6ac314a05dbe6d20823d8eed2
megumi <megusugizaki@gmail.com>
2024-02-08 11:13:31 +0900

    5-13

--- src/sukkiri/Main.java
+++ src/sukkiri/Main.java
@@ -1,15 +1,18 @@
 package sukkiri;
 
 public class Main {
-		 public static void printArray(int[] array) {
-			 for(int element: array) {
-				 System.out.println(element);
+		 public static void incArray(int[] array) {
+			 for (int i = 0; i < array.length; i ++) {
+				 array[i]++;
 			 }
 		 }
 		 
-		 public static void main (String[] args) {
-			 int array[] = {1, 2, 3};
-			 printArray(array);
+		 public static void main(String[] args) {
+			 int[] array = {1, 2, 3};
+			 incArray(array);
+			 for (int i : array) {
+				 System.out.println(i);
+			 }
 		 }
 
 	}
