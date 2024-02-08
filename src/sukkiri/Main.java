a10c43e9679ea6ee56aa6382f70928c901a5a6e7
megumi <megusugizaki@gmail.com>
2024-02-08 11:27:47 +0900

    5-14

--- src/sukkiri/Main.java
+++ src/sukkiri/Main.java
@@ -1,19 +1,18 @@
 package sukkiri;
 
 public class Main {
-		 public static void incArray(int[] array) {
-			 for (int i = 0; i < array.length; i ++) {
-				 array[i]++;
-			 }
-		 }
-		 
-//		 結果をreturnで返しておらず、incArrayではiに1を足す処理のみ。
-		 public static void main(String[] args) {
-			 int[] array = {1, 2, 3};
-			 incArray(array);
-			 for (int i : array) {
-				 System.out.println(i);
-			 }
-		 }
-
+	public static int[] makeArray(int size) {
+		int[] newArray = new int[size];
+		for(int i = 0; i < newArray.length; i ++) {
+			newArray[i] = i;
+		}
+		return newArray;
+	}
+	
+	public static void main(String[] args) {
+		int[] array = makeArray(3);
+		for (int i : array) {
+			System.out.println(i);
+		}
+	}
 	}
