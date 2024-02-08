46d1f8e7736b56c550995ca755037b549b4959d0
megumi <megusugizaki@gmail.com>
2024-02-08 12:08:14 +0900

    practice5-3

--- src/sukkiri/Main.java
+++ src/sukkiri/Main.java
@@ -7,7 +7,15 @@ public class Main {
 		System.out.println("本文:" + text);
 	}
 	
+	public static void email(String address, String text) {
+		System.out.println(address + "に、以下のメールを送信しました");
+		System.out.println("件名：無題");
+		System.out.println("本文：" + text);
+	}
+	
 	public static void main(String[] args) {
 		email("問い合わせ", "admin@admin", "本日は何時からですか");
+		System.out.println("-------------------------------");
+		email("address@address", "集合場所を教えてください");
 	}
 }
