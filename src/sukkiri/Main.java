3cc37f6d02f206b737a9fde39791943b21e0095d
megumi <megusugizaki@gmail.com>
2024-02-09 11:10:54 +0900

    6-4

--- src/sukkiri/Main.java
+++ src/sukkiri/Main.java
@@ -3,8 +3,8 @@ package sukkiri;
 public class Main {
 	public static void main(String[] args) {
 		int a = 10; int b = 2;
-		int total = tasu(a, b);
-		int delta = hiku(a, b);
+		int total = CalcLogic.tasu(a, b);
+		int delta = CalcLogic.hiku(a, b);
 		System.out.println("足すと" + total + "、引くと" + delta);
 	}
 	
