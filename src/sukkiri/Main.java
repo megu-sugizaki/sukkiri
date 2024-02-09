8b93e5877e2f75d436d7188a7ba5e6cbf97effac
megumi <megusugizaki@gmail.com>
2024-02-09 10:57:37 +0900

    6-1

--- src/sukkiri/Main.java
+++ src/sukkiri/Main.java
@@ -1,22 +1,19 @@
 package sukkiri;
 
 public class Main {
-	public static double calcTriangleArea(double length, double height) {
-		return length * height / 2;
-	}
-	
-	public static double calcCircleArea(double radius, final double PI) {
-		return radius * radius * PI;
-	}
-	
 	public static void main(String[] args) {
-		double length = 2;
-		double height = 3;
-		double radius = 2;
-		final double PI = 3.14;
-		System.out.println("三角形の底辺の長さが" + length + "cm、高さが" + height + "cmの場合、面積は" + calcTriangleArea(length, height) + "㎠");
-		System.out.println("円の半径が" + radius + "cmの場合、面積は" + calcCircleArea(radius, PI) + "㎠");
-		
+		int a = 10; int b = 2;
+		int total = tasu(a, b);
+		int delta = hiku(a, b);
+		System.out.println("足すと" + total + "、引くと" + delta);
+	}
+	
+	public static int tasu(int a, int b) {
+		return (a + b);
+	}
+	
+	public static int hiku(int a, int b) {
+		return (a - b);
 	}
 	
 }
