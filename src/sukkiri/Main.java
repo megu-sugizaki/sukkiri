ee2064193115f87213460f84d758b52f41be7634
megumi <megusugizaki@gmail.com>
2024-02-10 13:16:42 +0900

    6-10

--- src/sukkiri/Main.java
+++ src/sukkiri/Main.java
@@ -1,13 +1,11 @@
 package sukkiri;
 
-public class Main {
+public class Main{
 	public static void main(String[] args) {
-		int a = 10; int b = 2;
-		int total = CalcLogic.tasu(a, b);
-		int delta = CalcLogic.hiku(a, b);
-		System.out.println("足すと" + total + "、引くと" + delta);
+		int[] heights = {172, 149, 152, 191, 155};
+		java.util.Arrays.sort(heights);
+		for(int h : heights) {
+			System.out.println(h);
+		}
 	}
-	
-	
-	
 }
