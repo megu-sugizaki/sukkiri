38f99fb6edede4583cccf14e93e107bdd7eb0519
megumi <megusugizaki@gmail.com>
2024-02-12 11:45:05 +0900

    9-2

--- src/sukkiri/Main.java
+++ src/sukkiri/Main.java
@@ -2,21 +2,26 @@ package sukkiri;
 
 public class Main{
 	public static void main(String[] args) {
-		Hero h = new Hero();
-		h = new Hero();
-		h.hp = 100;
+		Hero h1;
+		h1 = new Hero();
+		h1.hp = 100;
+		Hero h2;
+		h2 = h1;
+		h2.hp = 200;
+		System.out.println(h1.hp);
 		
-		Matango m1 = new Matango();
-		m1.hp = 50;
-		m1.suffix = 'A';
 		
-		Matango m2 = new Matango();
-		m2.hp = 48;
-		m2.suffix = 'B';
-		
-		h.slip();
-		m1.run();
-		m2.run();
-		h.run();
+//		Matango m1 = new Matango();
+//		m1.hp = 50;
+//		m1.suffix = 'A';
+//		
+//		Matango m2 = new Matango();
+//		m2.hp = 48;
+//		m2.suffix = 'B';
+//		
+//		h.slip();
+//		m1.run();
+//		m2.run();
+//		h.run();
 	}
 }
