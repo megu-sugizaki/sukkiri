bbb1fe70cc5e39e26c1c8f1f00bdd95f35b5dd38
megumi <megusugizaki@gmail.com>
2024-02-15 17:56:12 +0900

    10-3

--- src/sukkiri/SuperHero.java
+++ src/sukkiri/SuperHero.java
@@ -1,19 +1,19 @@
 package sukkiri;
 
-public class SuperHero {
-	String name = "ミナト";
-	int hp = 100;
+public class SuperHero extends Hero {
+//	String name = "ミナト";
+//	int hp = 100;
 	boolean flying;
-	
-	public void attack(Matango m) {
-		System.out.println(this.name + "の攻撃！");
-		m.hp -= 5;
-		System.out.println("5ポイントのダメージを与えた");
-	}
-	
-	public void run() {
-		System.out.println(this.name + "は逃げ出した!");
-	}
+//	
+//	public void attack(Matango m) {
+//		System.out.println(this.name + "の攻撃！");
+//		m.hp -= 5;
+//		System.out.println("5ポイントのダメージを与えた");
+//	}
+//	
+//	public void run() {
+//		System.out.println(this.name + "は逃げ出した!");
+//	}
 	
 	public void fly() {
 		this.flying = true;
