aa43f128e0d0c84ce4aded11c4141453d28cf439
megumi <megusugizaki@gmail.com>
2024-02-15 17:57:51 +0900

    10-4

--- src/sukkiri/Main.java
+++ src/sukkiri/Main.java
@@ -2,40 +2,8 @@ package sukkiri;
 
 public class Main{
 	public static void main(String[] args) {
-		Sword s = new Sword();
-		s.name = "炎の剣";
-		s.damage = 10;
-		Hero h = new Hero("ミナト");
-		h.name = "ミナト";
-//		ここにsを代入しているのがいまいちわからない。→h.swordというのは、Swordの新しいインスタンスを作っている。そのSwordの名前は炎の剣。
-//		今のままだとSwordはあるけれどhに紐づいていない状態。hのために新しくSwordを生んだ。
-		h.sword = s;
-		System.out.println("現在の武器は" + h.sword.name);
-		System.out.println(h.name + "は" + h.sword.name + "で攻撃した");
+	SuperHero sh = new SuperHero();
+	sh.run();
 		
-		Hero h1 = new Hero();
-		Hero h2 = new Hero("アサカ");
-		Wizard w = new Wizard();
-		w.name = "スガワラ";
-		w.hp = 50;
-		
-		w.heal(h1);
-		w.heal(h2);
-		w.heal(h2);
-		
-		System.out.println(h.hp);
-		
-//		Matango m1 = new Matango();
-//		m1.hp = 50;
-//		m1.suffix = 'A';
-//		
-//		Matango m2 = new Matango();
-//		m2.hp = 48;
-//		m2.suffix = 'B';
-//		
-//		h.slip();
-//		m1.run();
-//		m2.run();
-//		h.run();
 	}
 }
