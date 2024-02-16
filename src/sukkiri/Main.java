57be13c74fbc57e57b2507c1ab303f00f5137d1e
megumi <megusugizaki@gmail.com>
2024-02-16 16:39:45 +0900

    10-11

--- src/sukkiri/Main.java
+++ src/sukkiri/Main.java
@@ -2,10 +2,6 @@ package sukkiri;
 
 public class Main{
 	public static void main(String[] args) {
-	Hero h = new Hero();
-	h.run();
-	
-	SuperHero sh = new SuperHero();
-	sh.run();
+	Weapon w = new Weapon();
 	}
 }
