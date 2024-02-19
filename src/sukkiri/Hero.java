3822a64d7c9ec9c3b566120ccbbbbcafc36487d1
megumi <megusugizaki@gmail.com>
2024-02-19 22:00:41 +0900

    11-3

--- src/sukkiri/Hero.java
+++ src/sukkiri/Hero.java
@@ -1,22 +1,9 @@
 package sukkiri;
 
-public class Hero {
-	String name = "ミナト";
-	int hp = 100;
-	
+public class Hero extends Character {
 	public void attack(Matango m) {
 		System.out.println(this.name + "の攻撃");
-		m.hp -= 5;
-		System.out.println("5ポイントのダメージを与えた！");
-	}
-	
-	public void run() {
-		System.out.println(this.name + "は逃げ出した！");
-	}
-	
-	public final void slip() {
-		this.hp -= 5;
-		System.out.println(this.name + "は転んだ!");
-		System.out.println("5のダメージ！");
+		System.out.println("敵に10ポイントのダメージを与えた");
+		m.hp -= 10;
 	}
 }
