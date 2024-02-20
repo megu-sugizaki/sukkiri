93b7991300678dc076695de416d93f5ce956d738
megumi <megusugizaki@gmail.com>
2024-02-20 21:51:09 +0900

    12-1

--- src/sukkiri/Main.java
+++ src/sukkiri/Main.java
@@ -2,9 +2,5 @@ package sukkiri;
 
 public class Main{
 	public static void main(String[] args) {
-	Weapon w = new Weapon();
-	}
-	
-	Hero h = new Hero();
-	PoisonMatango mp = new PoisonMatango('A');
+	Life lf = new Wizard();
 }
