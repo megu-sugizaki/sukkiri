2aca252c9a7730bfd5f0e1eb33bedc9bae3f2af3
megumi <megusugizaki@gmail.com>
2024-02-22 22:36:40 +0900

    12-3

--- src/sukkiri/Main.java
+++ src/sukkiri/Main.java
@@ -1,6 +1,11 @@
 package sukkiri;
 
 public class Main{
-	public static void main(String[] args) {
-	Life lf = new Wizard();
+	public static void main (String[] args) {
+		Wizard w = new Wizard();
+		Matango m = new Matango();
+		w.name = "アカリ";
+		w.attack(m);
+		w.fireball(m);
+	}
 }
