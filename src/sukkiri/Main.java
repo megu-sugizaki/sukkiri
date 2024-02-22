927ac158d93ec92631da566c7dbd08adfe9a4be2
megumi <megusugizaki@gmail.com>
2024-02-22 22:57:19 +0900

    12-9

--- src/sukkiri/Main.java
+++ src/sukkiri/Main.java
@@ -2,16 +2,13 @@ package sukkiri;
 
 public class Main{
 	public static void main (String[] args) {
-		Character[] c = new Character[5];
-		
-		c[0] = new Hero();
-		c[1] = new Hero();
-		c[2] = new Thief();
-		c[3] = new Wizard();
-		c[4] = new Wizard();
-		
-		for (Character ch : c) {
-			ch.hp += 50;
+		Monster[] monsters = new Monster[3];
+		monsters[0] = new Slime();
+		monsters[1] = new Goblin();
+		monsters[2] = new DeathBad();
+		for (Monster m : monsters) {
+			m.run();
 		}
+		
 	}
 }
