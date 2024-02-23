735c621bd3ea1c84c9c70c98fe1d9d4666a66b35
megumi <megusugizaki@gmail.com>
2024-02-23 22:19:18 +0900

    13-14

--- src/sukkiri/Main.java
+++ src/sukkiri/Main.java
@@ -2,12 +2,7 @@ package sukkiri;
 
 public class Main{
 	public static void main (String[] args) {
-		Y[] array = new Y[2];
-		array [0] = new A();
-		array [1] = new B();
-		
-		for (Y y : array) {
-			y.b();
-		}
+	Hero h = new Hero();
+	h.setName("");
 	}
 }
