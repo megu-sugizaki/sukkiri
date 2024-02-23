aee1125e1f43818b31887da66db1689e679afa81
megumi <megusugizaki@gmail.com>
2024-02-23 20:57:05 +0900

    13-4

--- src/sukkiri/Hero.java
+++ src/sukkiri/Hero.java
@@ -1,30 +1,12 @@
 package sukkiri;
 
 public class Hero{
-	int hp;
+	private int hp;
 	String name;
 	Sword sword;
 	
-	public void bye() {
-		System.out.println("勇者は別れをつげた");
-	}
-	
-	public void die () {
-		System.out.println(this.name + "は死んでしまった！");
-		System.out.println("GAMEOVERです");
-	}
-	
 	public void sleep() {
 		this.hp = 100;
-		System.out.println(this.name + "は眠って回復した");
-	}
-	
-	public void attack(Matango m) {
-		System.out.println(this.name + "の攻撃！");
-		System.out.println("お化けキノコ" + m.suffix + "から2ポイント");
-		this.hp -= 2;
-		if (this.hp <= 0) {
-			this.die();
-		}
+		System.out.println(this.name + "はって回復した");
 	}
 }
