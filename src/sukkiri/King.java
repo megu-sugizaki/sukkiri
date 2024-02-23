94e84b7bc9221d4eb93387c3ef41a0c581e87cb9
megumi <megusugizaki@gmail.com>
2024-02-23 22:05:45 +0900

    13-7

--- src/sukkiri/King.java
+++ src/sukkiri/King.java
@@ -2,9 +2,6 @@ package sukkiri;
 
 public class King {
 	public void talk(Hero h) {
-		System.out.println("王様：ようこそ我が国へ、勇者" + h.name  + "よ");
-		System.out.println("王様：長旅疲れたであろう");
-		System.out.println("王様：まずは城下町を見てくると良い。ではまた会おう");
-		h.die();
+		System.out.println("ようこそ我が国へ、勇者" + h.name + "よ");
 	}
 }
