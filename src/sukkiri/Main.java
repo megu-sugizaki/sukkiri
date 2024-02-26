9b61f0b735e4258aee563eba1b473d111caa9d65
megumi <megusugizaki@gmail.com>
2024-02-26 14:35:25 +0900

    14-2

--- src/sukkiri/Main.java
+++ src/sukkiri/Main.java
@@ -2,8 +2,8 @@ package sukkiri;
 
 public class Main{
 	public static void main (String[] args) {
-	Empty e = new Empty();
-	String s = e.toString();
-	System.out.println(s);
+	Object o1 = new Empty();
+	Object o2 = new Hero();
+	Object o3 = "こんにちは";
 }
 }
