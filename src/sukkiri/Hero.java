b84bd87fd51d55aeeaba9d8973634b7b6d1a6f0e
megumi <megusugizaki@gmail.com>
2024-02-27 13:29:29 +0900

    14-15

--- src/sukkiri/Hero.java
+++ src/sukkiri/Hero.java
@@ -6,6 +6,7 @@ public class Hero{
 	static int money;
 	
 	public static void setRandomMoney() {
-		Hero.money = (int)(Math.random() * 1000);
+		Hero.money = (int)(Math.random()*1000);
+		System.out.println(this.name + "たちの所持金を初期化しました");
 	}
 }
