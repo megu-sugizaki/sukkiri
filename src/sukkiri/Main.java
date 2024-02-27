0cd97c7e66e18d6536a129004db69af86f48155c
megumi <megusugizaki@gmail.com>
2024-02-27 13:00:53 +0900

    14-4

--- src/sukkiri/Main.java
+++ src/sukkiri/Main.java
@@ -1,7 +1,10 @@
 package sukkiri;
 
 public class Main{
-	public static void main (String[] args) {
-	System.out.println(o.toString());
-}
+	public static void main(String argue[]) {
+	Hero h = new Hero();
+	h.name = "ミナト";
+	h.hp = 100;
+	System.out.println(h.toString());
+	}
 }
