943eecf1f110e84839e7a57ab28de6f2d27ddd28
megumi <megusugizaki@gmail.com>
2024-02-27 13:11:38 +0900

    14-10

--- src/sukkiri/Main.java
+++ src/sukkiri/Main.java
@@ -2,16 +2,9 @@ package sukkiri;
 
 public class Main{
 	public static void main(String argue[]) {
-	Hero h1 = new Hero();
-	h1.name = "ミナト";
-	h1.hp = 100;
-	Hero h2 = new Hero();
-	h2.name = "ミナト";
-	h2.hp = 100;
-	if(h1.equals(h2) == true) {
-		System.out.println("同じ内容です");
-	} else {
-		System.out.println("違う内容です");
-	}
+		Hero h1 = new Hero();
+		Hero h2 = new Hero();
+		System.out.println(h1.hp);
+		System.out.println(Hero.money);
 	}
 }
