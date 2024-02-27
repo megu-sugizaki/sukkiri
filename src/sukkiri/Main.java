c89bec6deac259360611f3fea420b924d286dc28
megumi <megusugizaki@gmail.com>
2024-02-27 13:27:00 +0900

    14-14

--- src/sukkiri/Main.java
+++ src/sukkiri/Main.java
@@ -2,7 +2,9 @@ package sukkiri;
 
 public class Main{
 	public static void main(String argue[]) {
-		Hero.money = 100;
+		Hero.setRandomMoney();
 		System.out.println(Hero.money);
+		Hero h1 = new Hero();
+		System.out.println(h1.money);
 	}
 }
