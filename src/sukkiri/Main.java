1d9dd5bded2ac75e90ea6d64bf6c11df048b0ae4
megumi <megusugizaki@gmail.com>
2024-02-28 11:18:09 +0900

    15-5

--- src/sukkiri/Main.java
+++ src/sukkiri/Main.java
@@ -2,10 +2,19 @@ package sukkiri;
 
 public class Main{
 	public static void main(String argue[]) {
-		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < 1000; i ++) {
-			sb.append("Java");
-		}
-		String s = sb.toString();
+		public boolean isValidPlayerName(String name) {
+			if(name.length() != 8) {
+				return false;
+			}
+			char first = name.charAt(0);
+			if(!(first >= 'A' && first <= 'Z')) {
+				return false;
+			}
+			for (int i; i < 8; i++) {
+				char c = name.charAt(i);
+				if(!((c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9'))) {
+					return false;
+				}
+			}
 	}
 }
