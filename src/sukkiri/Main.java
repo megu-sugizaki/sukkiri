1aa34f6ad01b4054c12bc22490e5207890a314e4
megumi <megusugizaki@gmail.com>
2024-02-28 11:19:32 +0900

    15-6

--- src/sukkiri/Main.java
+++ src/sukkiri/Main.java
@@ -3,18 +3,7 @@ package sukkiri;
 public class Main{
 	public static void main(String argue[]) {
 		public boolean isValidPlayerName(String name) {
-			if(name.length() != 8) {
-				return false;
-			}
-			char first = name.charAt(0);
-			if(!(first >= 'A' && first <= 'Z')) {
-				return false;
-			}
-			for (int i; i < 8; i++) {
-				char c = name.charAt(i);
-				if(!((c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9'))) {
-					return false;
-				}
-			}
+			return name.matches("[A-Z][A-Z0-9]{7}");
+		}
 	}
 }
