9ace54ec2b5ceaa4cba5ce5c61ac6ae5a77a1fb9
megumi <megusugizaki@gmail.com>
2024-02-28 10:51:04 +0900

    15-3

--- src/sukkiri/Main.java
+++ src/sukkiri/Main.java
@@ -2,14 +2,8 @@ package sukkiri;
 
 public class Main{
 	public static void main(String argue[]) {
-		String s1 = "Java and JavaScript";
-		if (s1.contains("Java")) {
-			System.out.println("文字列sは、Javaを含んでいます");
-		}
-		if (s1.endsWith("Java")) {
-			System.out.println("文字列s1は。Javaが末尾にあります");
-		}
-		System.out.println("文字列sでJavaが最初に登場する位置は" + s1.indexOf("Java"));
-		System.out.println("文字列sでJavaが最後に登場する位置は" + s1.lastIndexOf("Java"));
+		String s1 = "Java programming";
+		System.out.println("文字列s1の4文字目以降は" + s1.substring(3));
+		System.out.println("文字列1の4~8文字目は" + s1.substring(3,8));
 	}
 }
