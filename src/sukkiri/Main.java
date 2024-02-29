2e3be8ffb1da83360f345edfe26da19a97e0fc6b
megumi <megusugizaki@gmail.com>
2024-03-01 08:44:07 +0900

    15-14

--- src/sukkiri/Main.java
+++ src/sukkiri/Main.java
@@ -1,17 +1,14 @@
 package sukkiri;
-import java.util.Calendar;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Main{
-	public static void main(String argue[]) {
-		Calendar c = Calendar.getInstance();
-		c.set(2019,8,22,1,23,45);
-		c.set(Calendar.MONTH, 9);
-		Date d = c.getTime();
+	public static void main(String args[]) throws Exception{
+		SimpleDateFormat f = new SimpleDateFormat("yyyy/mm/dd HH:mm:ss");
+		Date d = f.parse("2020/9/22 01:23:45");
 		System.out.println(d);
 		Date now = new Date();
-		c.setTime(now);
-		int y = c.get(Calendar.YEAR);
-		System.out.println("今年は" + y + "年です");
+		String s = f.format(now);
+		System.out.println("現在は" + s + "です");
 	}
 	}
