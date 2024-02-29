4ca8cf46bfb53f7c557104bc67c60bddfcdea3d0
megumi <megusugizaki@gmail.com>
2024-03-01 08:40:19 +0900

    15-12

--- src/sukkiri/Main.java
+++ src/sukkiri/Main.java
@@ -1,12 +1,15 @@
 package sukkiri;
+import java.util.Calendar;
 import java.util.Date;
 
 public class Main{
 	public static void main(String argue[]) {
+		Calendar c = Calendar.getInstance();
+		c.set(2019,8,22,1,23,45);
+		c.set(Calendar.MONTH, 9);
+		Date d = c.getTime();
+		System.out.println(d);
 		Date now = new Date();
-		System.out.println(now);
-		System.out.println(now.getTime());
-		Date past = new Date(1600705425827L);
-		System.out.println(past);
+		c.setTime(now);
 	}
 	}
