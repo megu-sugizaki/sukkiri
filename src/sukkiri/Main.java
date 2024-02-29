5b3edf0b1b0252eebbafaf02ab150288386eb415
megumi <megusugizaki@gmail.com>
2024-03-01 08:37:50 +0900

    15-11

--- src/sukkiri/Main.java
+++ src/sukkiri/Main.java
@@ -1,9 +1,12 @@
 package sukkiri;
+import java.util.Date;
 
 public class Main{
 	public static void main(String argue[]) {
-		long start = System.currentTimeMillis();
-		long end = System.currentTimeMillis();
-		System.out.println("処理にかかった時間は…" + (end - start) + "ミリ秒でした");
-		}
+		Date now = new Date();
+		System.out.println(now);
+		System.out.println(now.getTime());
+		Date past = new Date(1600705425827L);
+		System.out.println(past);
+	}
 	}
