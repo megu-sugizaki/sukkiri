13e8e5d1850b3669f031f61d06bdbb83495ffbe2
megumi <megusugizaki@gmail.com>
2024-03-04 07:12:36 +0900

    15-15-2

--- src/sukkiri/Main.java
+++ src/sukkiri/Main.java
@@ -1,25 +1,13 @@
 package sukkiri;
-import java.time.Instant;
+import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 
 public class Main{
 	public static void main(String args[]){
-		Instant il = Instant.now();
-		
-		Instant i2 = Instant.ofEpochMilli(1600705425827L);
-		long l = i2.toEpochMilli();
-		
-		ZonedDateTime z1 = ZonedDateTime.now();
-		ZonedDateTime z2 = ZonedDateTime.of(2020, 1,2,3,4,5,6, ZoneId.of("Asia/Tokyo"));
-		
-		Instant i3 = z2.toInstant();
-		ZonedDateTime z3 = i3.atZone(ZoneId.of("Europe/London"));
-		
-		System.out.println("東京:" + z2.getYear() + z2.getMonth() + z2.getDayOfMonth());
-		System.out.println("ロンドン" + z3.getYear() + z3.getMonth() + z3.getDayOfMonth());
-		if (z2.isEqual(z3)) {
-			System.out.println("これらは同じ時間をさしています");
-		}
+		LocalDateTime l1 = LocalDateTime.now();
+		LocalDateTime l2 = LocalDateTime.of(2020,1,1,9,5,0,0);
+		ZonedDateTime z1 = l2.atZone(ZoneId.of("Europe/London"));
+		LocalDateTime l3 = z1.toLocalDateTime();		
 	}
 	}
