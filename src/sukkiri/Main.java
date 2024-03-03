92099a86e3288b13250afa2d00689cdae87e2383
megumi <megusugizaki@gmail.com>
2024-03-04 08:10:27 +0900

    16-8

--- src/sukkiri/Main.java
+++ src/sukkiri/Main.java
@@ -1,18 +1,18 @@
 package sukkiri;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
+
+class Hero{
+	public String name;
+}
 
 public class Main{
 	public static void main (String[] args) {
-		Map<String, Integer>prefs = new HashMap<String, Integer>();
-		prefs.put("京都府", 255);
-		prefs.put("東京都", 1261);
-		prefs.put("熊本県", 181);
-		int tokyo = prefs.get("東京都");
-		System.out.println("東京都の人口は" + tokyo);
-		prefs.remove("京都府");
-		prefs.put("熊本県", 182);
-		int kumamoto = prefs.get("熊本県");
-		System.out.println("熊本県の人口は" + kumamoto);
+		Hero h = new Hero();
+		h.name = "ミナト";
+		List<Hero> list = new ArrayList<Hero>();
+		list.add(h);
+		h.name = "スガワラ";
+		System.out.println(list.get(0).name);
 	}
 }
