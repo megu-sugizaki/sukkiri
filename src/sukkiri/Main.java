234794c1e8b5976f2e7e1c0c12e9a8b29eec25ec
megumi <megusugizaki@gmail.com>
2024-03-04 07:56:16 +0900

    16-5

--- src/sukkiri/Main.java
+++ src/sukkiri/Main.java
@@ -1,17 +1,16 @@
 package sukkiri;
-import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class Main{
 	public static void main (String[] args) {
-		Set<String> colors = new HashSet<String>();
-		colors.add("赤");
-		colors.add("青");
-		colors.add("黄");
-		colors.add("赤");
-		
-		for (String s : colors) {
-			System.out.print(s + "→");
+		Set<String> words = new TreeSet<String>();
+		words.add("dog");
+		words.add("cat");
+		words.add("wolf");
+		words.add("panda");
+		for(String s : words) {
+			System.out.println(words + "→");
 		}
 	}
 }
