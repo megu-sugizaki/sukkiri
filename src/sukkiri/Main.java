140948ba9eadf809e210639cce13a5500394e6d0
megumi <megusugizaki@gmail.com>
2024-03-05 07:04:29 +0900

    17-1-2

--- src/sukkiri/Main.java
+++ src/sukkiri/Main.java
@@ -1,10 +1,6 @@
 package sukkiri;
 import java.io.FileWriter;
 
-class Hero {
-	public String name;
-}
-
 public class Main{
 	public static void main (String[] args) {
 		FileWriter fw = new FileWriter("data.text");
