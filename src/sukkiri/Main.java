ba9a87315175f2483f3484ca6d4e54e2dea0087a
megumi <megusugizaki@gmail.com>
2024-03-05 07:10:15 +0900

    17-4

--- src/sukkiri/Main.java
+++ src/sukkiri/Main.java
@@ -3,12 +3,13 @@ import java.io.FileWriter;
 
 public class Main{
 	public static void main (String[] args) {
+		FileWriter fw = null;
 		try {
-			FileWriter fw = new FileWriter("data.text");
+			fw = new FileWriter("data.text");
 			fw.write("hello");
-			fw.close();
 		} catch (Exception e) {
-			System.out.println("何らかの例外が発生しました");
+			System.out.println("エラーです");
 		}
+		fw.close();
 	}
 }
