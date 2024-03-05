e880ed41c02084f98851fd0256230724c71f8d58
megumi <megusugizaki@gmail.com>
2024-03-06 07:29:52 +0900

    17-12

--- src/sukkiri/Main.java
+++ src/sukkiri/Main.java
@@ -1,23 +1,6 @@
 package sukkiri;
-import java.io.FileWriter;
-import java.io.IOException;
 
 public class Main{
-	public static void main (String[] args) {
-		FileWriter fw = null;
-		try {
-			fw = new FileWriter("data.text");
-			fw.write("hello");
-		} catch (Exception e) {
-			System.out.println("何らかの例外が発生しました");
-		} finally {
-			if(fw != null) {
-				try {
-					fw.close();
-				} catch (IOException e){
-					;
-				}
-			}
-		} 
-	}
+	Person p = new Person();
+	p.setAge(-128);
 }
---
+++ src/sukkiri/Person.java
@@ -0,0 +1,12 @@
+package sukkiri;
+
+public class Person {
+	int age;
+	
+	public void setAge(int age) {
+		if (age < 0) {
+			throw new IllegalArgumentException("年齢は正の数を指定すべきです。指定値＝" + age);
+		}
+		this.age = age;
+	}
+}
