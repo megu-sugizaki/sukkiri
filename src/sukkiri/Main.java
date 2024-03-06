27ff66ee0d06eb7c7b84478d9771d801df3da59a
megumi <megusugizaki@gmail.com>
2024-03-07 07:14:26 +0900

    18-2

--- src/sukkiri/Main.java
+++ src/sukkiri/Main.java
@@ -2,11 +2,10 @@ package sukkiri;
 import java.io.*;
 
 public class Main{
-	FileReader fr = new FileReader("data.txt");
-	int input = fr.read();
-	while (input != -1) {
-		System.out.print((char)input);
-		input = fr.read();
-	}
-	fr.close();
+	FileWriter fw = new FileWriter("data.txt");
+	fw.write('そ');
+	fw.write('れ');
+	fw.write('で');
+	fw.write('は');
+	fw.close;
 }
