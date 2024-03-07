a01255f8a7b6dd7bd7ee145053ff324091e0b7e8
megumi <megusugizaki@gmail.com>
2024-03-08 07:28:45 +0900

    18-5

--- src/sukkiri/Main.java
+++ src/sukkiri/Main.java
@@ -1,14 +1,18 @@
 package sukkiri;
-import java.sql.Connection;
-import java.sql.DriverManager;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class Main{
 	public static void main (String[] args) throws Exception{
-	Class.forName("org.h2.Driver");
-	String dburl = "jdbc:h2:~/test";
-	String sql = "INSERT INTO EMPLOYEES(name) VALUES('aoki')";
-	Connection conn = DriverManager.getConnection(dburl);
-	conn.createStatement().executeUpdate(sql);
-	conn.close();
+		JFrame frame = new JFrame("はじめてのGUI");
+		JLabel label = new JLabel("Hello World!");
+		JButton button = new JButton("押してね");
+		frame.getContentPane().setLayout(new_FlowLayout());
+		frame.getContentPane().add(label);
+		frame.getContentPane().add(button);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(300, 300);
+		frame.setVisible(true);
 	}
 }
